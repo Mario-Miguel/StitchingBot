08b20290cbf9ed0febd7f30b4fa53955d2ccbe54
Mario-Miguel <mariomig5@gmail.com>
2021-07-04 16:56:21 +0200

    Cambios en el controlador de arduino

--- Circuito/motor_controller/motor_controller.ino
+++ Circuito/motor_controller/motor_controller.ino
@@ -4,9 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
+//#####################################################################################
+
 //Joystick Threshold
 #define FORWARD_THRESHOLD 550
 #define REVERSE_THRESHOLD 450
+
+//Joystick
+#define vrX A0
+#define vrY A1
+#define jsSwitch 12
+
+//#####################################################################################
 
 //Motor states
 #define FORWARD 1
@@ -18,25 +27,29 @@
 #define C_REVERSE 0
 #define C_STOP 1
 
-//Joystick
-#define vrX A0
-#define vrY A1
-#define jsSwitch 12
-
 //StepperMotors
 #define DIRX  2
 #define STEPX 3
-#define DIRY  8
-#define STEPY 9
+
+#define DIRY_1  7
+#define STEPY_1 6
+#define DIRY_2  8
+#define STEPY_2 9
 
 #define motorInterfaceType 1
 
-
 //AccelStepper definition
 AccelStepper stepperX = AccelStepper(motorInterfaceType, STEPX, DIRX);
-AccelStepper stepperY = AccelStepper(motorInterfaceType, STEPY, DIRY);
+AccelStepper stepperY1 = AccelStepper(motorInterfaceType, STEPY_1, DIRY_1);
+AccelStepper stepperY2 = AccelStepper(motorInterfaceType, STEPY_2, DIRY_2);
 
+//#####################################################################################
 
+//Finales de carrera
+#define FIN_CARRX 25
+#define FIN_CARRY 23
+
+//#####################################################################################
 
 //Variables
 int xValue = STOP;
@@ -48,6 +61,7 @@ int yMotorState = 0;
 
 int state = 0;
 
+//#####################################################################################
 
 //Bluetooth
 #define rx 19 // => HC05: tx
@@ -55,14 +69,29 @@ int state = 0;
 
 SoftwareSerial BTSerial(rx, tx); // RX | TX of Arduino
 
-char command[100];
-char delimeter[] = ",;:";
+char command;
 
 
-int index = 0;
+//#####################################################################################
+const String ASK_FOR_ACTIONS = "M";
+#define MAX_ARRAY_SIZE 100
+String actionsRead[MAX_ARRAY_SIZE];
+int timeActionsRead[MAX_ARRAY_SIZE];
+int xCoords[MAX_ARRAY_SIZE];
+int yCoords[MAX_ARRAY_SIZE];
+int hasToChange[MAX_ARRAY_SIZE];
 
-char c;
-boolean flag = false;
+//Ejercicio 5.1
+int actionCounter = 0;
+unsigned long startActionTime = 0;
+boolean entradaLeida = false;
+boolean startedFromJava = false;
+boolean areActionsRemaining = false;
+boolean actualPedalState = true;
+
+String receivedString = "";
+
+//#####################################################################################
 
 void setup() {
 
@@ -70,23 +99,27 @@ void setup() {
   //Serial turns on in 1 second
   delay(1000);
   Serial1.begin(9600);
-  
-//  pinMode(ledPin, OUTPUT);
-//  digitalWrite(ledPin, LOW);
+  Serial.begin(4800);
+
+  //  pinMode(ledPin, OUTPUT);
+  //  digitalWrite(ledPin, LOW);
+  pinMode(FIN_CARRY, INPUT);
+  pinMode(FIN_CARRX, INPUT);
 
 
   //Joystick
   pinMode(jsSwitch, INPUT);
 
   //Stepper motors
-  stepperX.setMaxSpeed(1000);
-  stepperY.setMaxSpeed(1000);
+  stepperX.setMaxSpeed(5000);
+  stepperY1.setMaxSpeed(5000);
+  stepperY2.setMaxSpeed(5000);
 
 }
 
 void loop() {
 
-  bluetoothRead();
+  bluetoothRead2();
 
   //getJoystickValues();
 
@@ -96,96 +129,150 @@ void loop() {
 
 }
 
-void bluetoothRead() {
+//#####################################################################################
 
-  
+//void bluetoothRead() {
+//
+//  if (Serial1.available() > 0) {
+//    command = Serial1.read();
+//    Serial.println(command);
+//    if (command == 'R') {
+//      moveMotorsLib4(xCoords[actionCounter] + 1, yCoords[actionCounter]);
+//    }
+//    else if (command == 'L') {
+//      moveMotorsLib4(xCoords[actionCounter] - 1, yCoords[actionCounter]);
+//    }
+//    else if (command == 'U') {
+//      moveMotorsLib4(xCoords[actionCounter], yCoords[actionCounter] + 1);
+//    }
+//    else if (command == 'D') {
+//      moveMotorsLib4(xCoords[actionCounter], yCoords[actionCounter] - 1);
+//    }
+//
+//    delay(10);
+//
+//  }
+//
+//}
 
-if (Serial1.available()>0) {
-    command = Serial1.read();
-    //V,1
-    //V,-1
-    //H,1
-    //H,-1
-    //H,0
-    //V,0
 
-    //<- H,-1;V,0
-    //-> H,1;V,0
-    //^ H,0;V,1
-    //_ H,0;V,-1
+void bluetoothRead2() {
+  if (Serial1.available() > 0) {
+    receivedString = Serial1.readString();
+    createActions(receivedString);
+    areActionsRemaining = true;
+    delay(100);
+  }
 
-    //First motor order 
-    char* ptr = strtok(string, delimiter);
+  //---------------------------------------
 
-    while(ptr != NULL) {
-      
-        Serial1.println(">Motor: "+ ptr);
-        // create next part
-        ptr = strtok(NULL, delimiter);
+  if (areActionsRemaining) {
 
-        Serial1.println(">Direction: "+ ptr);
-        String horizontalDir =ptr;
-
-        ptr = strtok(NULL, delimiter);
-        Serial1.println(">Motor: "+ ptr);
-        
-        ptr = strtok(NULL, delimiter);
-        Serial1.println(">Direction: "+ ptr);
-        String verticalDir =ptr;
-
-        //Next order
-        ptr = strtok(NULL, delimiter);
+    if (actionCounter == MAX_ARRAY_SIZE) {
+      //      Serial.println(">" + ASK_FOR_ACTIONS);
+      Serial.println(">Pide más acciones");
+      Serial1.println(ASK_FOR_ACTIONS);
+      resetArrays();
+      actionCounter = 0;
+      entradaLeida = false;
+      delay(100);
     }
 
-    
-//    Serial1.println("Arduino dice: hey");
-//    if (command == "abrir") {
-//      Serial1.println("Arduino dice: puerta abierta");
-//      //digitalWrite(ledPin, HIGH);
-//    }
-//    if (command == "cerrar") {
-//      Serial1.println("Arduino dice: puerta cerrada");
-//      //digitalWrite(ledPin, LOW);
-//    }
+    if (entradaLeida) {
+      Serial.println(">Ejecuta una acción");
+      executeActions();
+    }
   }
-  
 }
 
-void processCommand() {
-//  char motor = datos[0];
-//  char inst = datos[1];
+//#####################################################################################
 
-//  int motorInstruction = 
-  
-//  switch (command) {
-//    case 'H':
-//      if (inst == C_) {
-//        //digitalWrite(ledPin, HIGH);
-//        Serial1.println("Light: ON");
-//      }
-//      else if (inst == 'N') {
-//        //digitalWrite(ledPin, LOW);
-//        Serial1.println("Light: OFF");
-//      }
-//      break;
-//  }
-  //  char command = datos[0];
-  //  char inst = datos[1];
-  //  switch (command) {
-  //    case 'L':
-  //      if (inst == 'Y') {
-  //        digitalWrite(LIGHT, HIGH);
-  //        btm.println("Light: ON");
-  //      }
-  //      else if (inst == 'N') {
-  //        digitalWrite(LIGHT, LOW);
-  //        btm.println("Light: OFF");
-  //      }
-  //      break;
-  //  }
+void createActions(String actionsString) {
+  String entrada = actionsString;
+  Serial.println(">Empieza a crear acciones con: " + actionsString);
+  String entradaPartida = "";
+  int i = 0;
+  do {
+    entradaPartida = getValue(entrada, ';', i);
+
+    if (entradaPartida != "") {
+
+      xCoords[i] = getValue(entradaPartida, ',', 0).toInt();
+      yCoords[i] = getValue(entradaPartida, ',', 1).toInt();
+      hasToChange[i] = getValue(entradaPartida, ',', 2).toInt();
+
+    }
+    i = i + 1;
+  } while (entradaPartida != "");
+
+  Serial.println(">Termina de crear acciones");
+  entradaLeida = true;
 }
 
-//################################################################################
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+//#####################################################################################
+
+void executeActions() {
+
+
+  if (xCoords[actionCounter] == 0 && yCoords[actionCounter] == 0) {
+    Serial.println(">Se acabaron las acciones wey");
+    Serial1.println(ASK_FOR_ACTIONS);
+    actionCounter = 0;
+  }
+  else {
+    //Si tiene que cambiar, parar el pedal, despues mover en los ejes que sean
+    if(hasToChange[actionCounter]){
+      setPedalState(false);
+    }
+    else{
+      setPedalState(true);
+    }
+    //Hacer que se mueva a la coordenada indicada
+    Serial.println(">Se mueve a la coordenada x: " + String(xCoords[actionCounter]) + " - y: " + String(yCoords[actionCounter]));
+    moveMotorsLib4(xCoords[actionCounter], yCoords[actionCounter]);
+    actionCounter++;
+  }
+}
+
+//#####################################################################################
+
+void setPedalState(boolean state){
+  
+  if(!state){
+    //TODO mirar a ver como controlar el pedal. En este caso hay que dejar de pisarlo
+    actualPedalState = state;
+  }
+  else if(!actualPedalState){
+    //TODO Hacer que se pulse el pedal
+    actualPedalState = true;
+  }
+}
+
+//#####################################################################################
+
+void resetArrays() {
+  memset(xCoords, 0, sizeof(xCoords));
+  memset(yCoords, 0, sizeof(yCoords));
+  memset(hasToChange, 0, sizeof(hasToChange));
+}
+
+//#####################################################################################
 
 void getJoystickValues() {
   xValue = analogRead(vrX);
@@ -195,17 +282,73 @@ void getJoystickValues() {
   yMotorState = getCurrentState(yValue);
 }
 
-//################################################################################
+//#####################################################################################
 
-void moveMotorsLib(int xState, int yState) {
-  stepperX.setSpeed(500 * xState);
-  stepperX.runSpeed();
+//void moveMotorsLib(int xState, int yState) {
+//  stepperX.setSpeed(250 * xState);
+//  if (!endOfPathX())
+//    stepperX.runSpeed();
+//
+//  stepperY1.setSpeed(250 * yState);
+//  stepperY2.setSpeed(250 * yState);
+//  if (!endOfPathY()) {
+//    stepperY1.runSpeed();
+//    stepperY2.runSpeed();
+//  }
+//
+//}
 
-  stepperY.setSpeed(500 * yState);
-  stepperY.runSpeed();
+
+void moveMotorsLib4(int xCoord, int yCoord) {
+  int xMovement = xCoord - stepperX.currentPosition();
+  int yMovement = yCoord - stepperY1.currentPosition();
+  int directionX = 0;
+  int directionY = 0;
+
+  if(xMovement<0){
+    directionX = -1;
+  }
+  else{
+    directionX = 1;
+  }
+  if(yMovement<0){
+    directionY = -1;
+  }
+  else{
+    directionY = 1;
+  }
+
+  stepperX.moveTo(xMovement);
+  stepperY1.moveTo(yMovement);
+  stepperY2.moveTo(yMovement);
+
+  stepperX.setSpeed(500 * directionX);
+  stepperY1.setSpeed(500 * directionY);
+  stepperY2.setSpeed(500 * directionY);
+
+  
+  while ((!endOfPathX() && stepperX.distanceToGo() != 0) || (!endOfPathY() && stepperY1.distanceToGo() != 0)){
+    if(!endOfPathX() && stepperX.distanceToGo() != 0)
+      stepperX.runSpeed();
+      
+    if(!endOfPathY() && stepperY1.distanceToGo() != 0){
+      stepperY1.runSpeed();
+      stepperY2.runSpeed();
+    }
+  }
+
 }
 
-//################################################################################
+//#####################################################################################
+
+bool endOfPathY() {
+  return !digitalRead(FIN_CARRY);
+}
+bool endOfPathX() {
+  return !digitalRead(FIN_CARRX);
+}
+
+//#####################################################################################
 
 int getCurrentState(int readValue) {
   if (readValue > FORWARD_THRESHOLD) {
--- StitchingBot/app/src/main/java/es/uniovi/eii/stitchingbot/translator/Translator.kt
+++ StitchingBot/app/src/main/java/es/uniovi/eii/stitchingbot/translator/Translator.kt
@@ -2,25 +2,19 @@ package es.uniovi.eii.stitchingbot.translator
 
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.os.Build
-import android.os.Build.VERSION.SDK_INT
 import android.util.Log
-import androidx.annotation.RequiresApi
-import java.nio.ByteBuffer
-import kotlin.experimental.and
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 const val TAG: String = "TranslateOrders"
 const val POINT: Int = 1
 const val HEIGHT: Int = 750
 const val WIDTH: Int = 750
+const val FACTOR_AJUSTE: Int = 5
 
 class Translator(private val image: Bitmap) {
 
     private lateinit var pointsMatrix: Array<IntArray>
 
-    fun run(): MutableList<Pair<Int, Int>> {
+    fun run(): MutableList<Triple<Int, Int, Boolean>> {
 
         Log.i(TAG, "${image.width} - ${image.height}")
         //La relacion es de 50 pixeles <-> 1mm
@@ -30,8 +24,6 @@ class Translator(private val image: Bitmap) {
         Log.i(TAG, "Scaled: ${scaledBitmap.height}-${scaledBitmap.width}")
 
 
-        var coords = mutableListOf<Pair<Int, Int>>()
-
         //Coordenada X, coordenada Y, ¿tiene que levantar el pedal para la siguiente?
         val coordsV2 = mutableListOf<Triple<Int, Int, Boolean>>()
 
@@ -40,244 +32,46 @@ class Translator(private val image: Bitmap) {
         }
 
 
+        for (y in 0 until scaledBitmap.height) {
+            for (x in 0 until scaledBitmap.width) {
+                val pixel = scaledBitmap.getPixel(x, y)
 
-
-            for (y in 0 until scaledBitmap.height) {
-                for (x in 0 until scaledBitmap.width) {
-                    val pixel = scaledBitmap.getPixel(x, y)
-
-                    if (pixel == Color.BLACK) {
-                        coords.add(Pair(x, y))
-                        coordsV2.add(Triple(x, y, false))
-                        pointsMatrix[y][x] = 1
-                    }
-
+                if (pixel == Color.BLACK) {
+                    //coords.add(Pair(x, y))
+                    coordsV2.add(Triple(x, y, false))
+                    pointsMatrix[y][x] = POINT
                 }
+
             }
+        }
 
 
-        var orderedArray = order(coords)
         var orderedArrayV2 = completeArray(coordsV2)
-        val orderedReducedArray = orderReduce(coords)
-
-        orderedArray = orderedArray.map{ Pair(it.first*5, it.second*5) }.toMutableList()
+        orderedArrayV2 = orderedArrayV2.map {
+            Triple(
+                it.first * FACTOR_AJUSTE,
+                it.second * FACTOR_AJUSTE,
+                it.third
+            )
+        }.toMutableList()
 
 
         Log.i(TAG, "End of processing")
 
-        return orderedArray
+        return orderedArrayV2
     }
 
     private fun completeArray(coordsV2: MutableList<Triple<Int, Int, Boolean>>): MutableList<Triple<Int, Int, Boolean>> {
+        val completedArray = mutableListOf<Triple<Int, Int, Boolean>>()
 
-        return mutableListOf()
-    }
-
-
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    private fun processImage(scaledBitmap: Bitmap): MutableList<Pair<Int, Int>> {
-//        val coords = mutableListOf<Pair<Int, Int>>()
-//
-//        //FORMA 1
-//        val size = scaledBitmap.byteCount
-//        val byteBuffer = ByteBuffer.allocate(size)
-//        scaledBitmap.copyPixelsToBuffer(byteBuffer)
-//        val byteArray = byteBuffer.array()
-//
-//        val master = Master(4, byteArray, HEIGHT, WIDTH)
-//        master.compute()
-//
-//        //master.compute2()
-//
-//
-//        coords.addAll(master.coordsArray)
-//
-//        return coords
-//
-//    }
-
-
-    private fun order(
-        coords: MutableList<Pair<Int, Int>>
-    ): MutableList<Pair<Int, Int>> {
-        val orderedArray = mutableListOf<Pair<Int, Int>>()
-
-        orderedArray.add(Pair(coords[0].first, coords[0].second))
-
-        var counter = 0
-        var xForward = true
-        var change = false
-        var yUp = false
-        var triedToChange = false
-
-        while (orderedArray.size != coords.size) {
-            val xCoord = orderedArray[counter].first
-            val yCoord = orderedArray[counter].second
-
-            val rightStatus = if(xCoord+1<pointsMatrix[0].size)pointsMatrix[yCoord][xCoord + 1] else 0
-            val leftStatus = if(xCoord-1>=0) pointsMatrix[yCoord][xCoord - 1] else 0
-            val upStatus = if(yCoord-1>=0) pointsMatrix[yCoord - 1][xCoord] else 0
-            val downStatus = if(yCoord+1<pointsMatrix.size) pointsMatrix[yCoord + 1][xCoord] else 0
-
-            //var currentStatus = pointsMatrix[yCoord][xCoord]
-
-
-            if (!change) {
-                //Si va pa la derecha -> x++
-                if (xForward) {
-                    if (xCoord + 1 < pointsMatrix[0].size && rightStatus == POINT) {
-                        orderedArray.add(Pair(xCoord + 1, yCoord))
-                        counter++
-                        triedToChange=false
-
-
-                    } else {
-                        //Cambiar la direccion en la que debe ir y cambiar a subir o bajar
-                        xForward = !xForward
-                        change = true
-                    }
-                }
-                //Si va pa la izquierda -> x--
-                else {
-                    if (xCoord - 1 >= 0 && leftStatus == POINT) {
-                        orderedArray.add(Pair(xCoord - 1, yCoord))
-                        counter++
-                        triedToChange=false
-
-                    } else {
-                        //Cambiar la direccion en la que debe ir y cambiar a subir o bajar
-                        xForward = !xForward
-                        change = true
-                    }
-                }
-            } else {
-                //Si va pa arriba -> y--
-                if (yUp) {
-                    //Si puede seguir subiendo
-                    if (yCoord - 1 >= 0 && upStatus == POINT && !triedToChange) {
-                        //Si encuentra un punto en el pixel superior
-                        orderedArray.add(Pair(xCoord, yCoord - 1))
-                        counter++
-                        triedToChange=true
-
-                    } else {
-                        //Si no lo encuentra en la parte superior
-                        yUp = !yUp
-                        if (xCoord + 1 < pointsMatrix.size && downStatus == POINT && !triedToChange) {
-                            //Si encuentra un punto en el pixel superior
-
-                            orderedArray.add(Pair(xCoord, yCoord + 1))
-                            counter++
-                            triedToChange=true
-
-                        } else {
-                            //No encuentra ni arriba, ni abajo, ni a los lados => Saltar a otra coordenada
-                            orderedArray.add(
-                                searchForNearestCoord(
-                                    xCoord,
-                                    yCoord,
-                                    coords,
-                                    orderedArray
-                                )
-                            )
-                            counter++
-                            triedToChange=false
-                        }
-                    }
-                }
-                //Si va pa abajo -> y++
-                else {
-                    //Si puede seguir bajando
-                    if (xCoord + 1 < pointsMatrix.size && downStatus == POINT&& !triedToChange) {
-                        //Si encuentra un punto en el pixel de abajo
-                        orderedArray.add(Pair(xCoord, yCoord + 1))
-                        counter++
-                        triedToChange=true
-
-                    } else {
-                        //Si no lo encuentra en la parte inferior
-                        yUp = !yUp
-                        if (yCoord - 1 >= 0 && upStatus == POINT&& !triedToChange) {
-                            //Si encuentra un punto en el pixel superior
-
-                            orderedArray.add(Pair(xCoord, yCoord - 1))
-                            counter++
-                            triedToChange=true
-
-                        } else {
-                            //No encuentra ni arriba, ni abajo, ni a los lados => Saltar a otra coordenada
-                            orderedArray.add(
-                                searchForNearestCoord(
-                                    xCoord,
-                                    yCoord,
-                                    coords,
-                                    orderedArray
-                                )
-                            )
-                            counter++
-                            triedToChange=false
-                        }
-                    }
-                }
-                change = false
-            }
-
-
-        }
-
-        return orderedArray
-
-    }
-
-    private fun searchForNearestCoord(
-        xCoord: Int,
-        yCoord: Int,
-        coords: MutableList<Pair<Int, Int>>,
-        orderedArray: MutableList<Pair<Int, Int>>
-    ): Pair<Int, Int> {
-        Log.i(TAG, "ME CAGO EN MI PUTA VIDA")
-        val minimumDistance = Double.MAX_VALUE
-        var actualDistance: Double
-        var nearestPair = Pair(0, 0)
-
-        for (coordinate in coords) {
-            if (!orderedArray.contains(coordinate)) {
-                actualDistance = sqrt(
-                    ((coordinate.second - yCoord).toDouble()).pow(2.0) + ((coordinate.first - xCoord).toDouble()).pow(
-                        2.0
-                    )
-                )
-
-                if (actualDistance < minimumDistance) {
-                    nearestPair = coordinate
-                }
+        for (i in 1 until coordsV2.size) {
+            //Si las coordenadas de x NO son contiguas
+            if (coordsV2[i - 1].first != coordsV2[i].first - 1) {
+                coordsV2[i] = Triple(coordsV2[i].first, coordsV2[i].second, true)
             }
         }
+        return completedArray
 
-        return nearestPair
     }
-
-    private fun orderReduce(coords: MutableList<Pair<Int, Int>>): MutableList<Pair<Int, Int>> {
-        val orderedArray = mutableListOf<Pair<Int, Int>>()
-        val firstXCoord = coords[0].first
-        val firstYCoord = coords[0].second
-        var i = 0
-        var j = 0
-        while (i in firstYCoord..pointsMatrix.size) {
-            while (j in firstXCoord..pointsMatrix[0].size) {
-                if (pointsMatrix[i][j] == POINT) {
-                    orderedArray.add(Pair(i, j))
-                    j += 50
-                }
-                j++
-            }
-            i += 50
-        }
-
-        return orderedArray
-    }
-
-//x=190, y=656
-
 
 }
