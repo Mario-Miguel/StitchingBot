227f4b930fd84a9e1551643f3634ace2645c916d
Mario-Miguel <mariomig5@gmail.com>
2021-11-07 17:03:56 +0100

    Prueba de tests

---
+++ StitchingBot/app/src/test/java/es/uniovi/eii/stitchingbot/LogoUnitTest.kt
@@ -0,0 +1,23 @@
+package es.uniovi.eii.stitchingbot
+
+import org.junit.Test
+
+class LogoUnitTest {
+
+    @Test
+    fun addLogoTest() {
+
+    }
+
+    @Test
+    fun modifyLogoTest() {
+
+    }
+
+    @Test
+    fun deleteLogoTest() {
+
+    }
+
+
+}
---
+++ StitchingBot/app/src/test/java/es/uniovi/eii/stitchingbot/TranslatorUnitTest.kt
@@ -0,0 +1,16 @@
+package es.uniovi.eii.stitchingbot
+
+import org.junit.Test
+
+class TranslatorUnitTest {
+
+    @Test
+    fun emptyLogoTranslationTest() {
+
+    }
+
+    @Test
+    fun logoTranslationTest() {
+
+    }
+}
