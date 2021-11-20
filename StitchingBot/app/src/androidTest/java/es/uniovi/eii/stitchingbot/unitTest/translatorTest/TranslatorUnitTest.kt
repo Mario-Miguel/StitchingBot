e62406354ca61445c58329228dcc0d4e17ea35ad
Mario-Miguel <mariomig5@gmail.com>
2021-11-20 13:09:00 +0100

    Cambios finales

--- StitchingBot/app/src/androidTest/java/es/uniovi/eii/stitchingbot/unitTest/translatorTest/TranslatorUnitTest.kt
+++ StitchingBot/app/src/androidTest/java/es/uniovi/eii/stitchingbot/unitTest/translatorTest/TranslatorUnitTest.kt
@@ -11,7 +11,7 @@ class TranslatorUnitTest {
     @Test
     fun emptyLogoTranslationTest() {
         val numOfPaintedRows = 0
-        val image = getCustomBitmap(numOfPaintedRows, 375, 375)
+        val image = getCustomBitmap(numOfPaintedRows, 250, 250)
         val translator = Translator
         translator.image = image
         translator.run {}
@@ -26,7 +26,7 @@ class TranslatorUnitTest {
     @Test
     fun logoTranslationTest() {
         val numOfPaintedRows = 5
-        val image = getCustomBitmap(numOfPaintedRows, 375, 375)
+        val image = getCustomBitmap(numOfPaintedRows, 250, 250)
         val translator = Translator
         translator.image = image
         translator.run {}
@@ -35,7 +35,7 @@ class TranslatorUnitTest {
         val assertFistCoord = Translator.calculateAdjustedCoord(Pair(0, 0))
 
         Assert.assertEquals(assertFistCoord, firstCoord)
-        Assert.assertEquals((375 * numOfPaintedRows / 2) - 1, translator.translation.size)
+        Assert.assertEquals((250 * numOfPaintedRows / 2) - 1, translator.translation.size)
     }
 
     private fun getCustomBitmap(numOfRows: Int, height: Int, width: Int): Bitmap {
@@ -43,7 +43,7 @@ class TranslatorUnitTest {
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
 
         val startPixel = 0
-        val endPixel = startPixel + (374 * numOfRows)
+        val endPixel = startPixel + (249 * numOfRows)
         for (i in startPixel..endPixel) {
             pixels[i] = Color.BLACK
         }
